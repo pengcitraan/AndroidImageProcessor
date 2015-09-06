98374d3f72f26db026d11bd62e07ec84d72cceb8
Yanfa Adi Putra <yanfa.adiputra@gmail.com>
2015-09-06 20:49:28 +0700

    udah ada algoritmanya cuman kayanya msh kurang n belom bisa adjust

--- app/src/main/java/com/example/yanfa/pengcit1/ImageProcessor.java
+++ app/src/main/java/com/example/yanfa/pengcit1/ImageProcessor.java
@@ -1,6 +1,11 @@
 package com.example.yanfa.pengcit1;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -59,5 +64,93 @@ public class ImageProcessor {
             }
         }
         return result;
+    }
+
+    public static Bitmap toGrayscale(Bitmap bmpOriginal){
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayscale;
+    }
+
+    public static Bitmap histogramEqualization(Bitmap bitmap, int adjustRed, int adjustCuf){
+        int redFreq[] = new int[256];
+        int redCuF[] = new int[256];
+        int feq[] = new int[256];
+        int cuFeq[] = new int[256];
+        int map[] = new int[256];
+
+        Bitmap greyscaleBitmap = toGrayscale(bitmap);
+
+        int totalPixel = greyscaleBitmap.getHeight() + greyscaleBitmap.getWidth();
+
+        int width, height;
+        height = greyscaleBitmap.getHeight();
+        width = greyscaleBitmap.getWidth();
+
+        Bitmap tempBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Bitmap hisEqBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++) {
+                int p = greyscaleBitmap.getPixel(i, j);
+                int R = (p & 0xff0000) >> 16;
+                redFreq[R]++;
+            }
+        }
+
+        redCuF[0] = redFreq[0] + adjustRed;
+        for(int i = 1; i < 256; i++){
+            redCuF[i] = redCuF[i-1] + redFreq[i];
+        }
+
+        for(int i = 0; i < 256; i++){
+            feq[i] = totalPixel / 256;
+        }
+
+        cuFeq[0] = feq[0] + adjustCuf;
+        for(int i = 1; i < 256; i++){
+            cuFeq[i] = cuFeq[i-1] + feq[i];
+        }
+
+        for(int i = 0; i < 256; i++){
+            int counter = 1;
+            int smallestDiff = Math.abs((redCuF[i] - cuFeq[0]));
+            while(counter < 256){
+                int tempDiff = Math.abs((redCuF[i] - cuFeq[counter]));
+                if(smallestDiff > tempDiff){
+                    smallestDiff = tempDiff;
+                    counter++;
+                }
+                if(smallestDiff <= tempDiff) {
+                    counter--;
+                    break;
+                }
+            }
+            map[i] = counter;
+        }
+
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++) {
+                int p = greyscaleBitmap.getPixel(i,j);
+                int R = (p & 0xff0000) >> 16;
+                int G = (p & 0x00ff00) >> 8;
+                int B = (p & 0x0000ff) >> 0;
+                tempBitmap.setPixel(i,j, Color.rgb(map[R], G, B));
+            }
+        }
+
+        hisEqBitmap = toGrayscale(tempBitmap);
+
+        return hisEqBitmap;
     }
 }
--- app/src/main/java/com/example/yanfa/pengcit1/MainActivity.java
+++ app/src/main/java/com/example/yanfa/pengcit1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.yanfa.pengcit1;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -19,16 +20,20 @@ public class MainActivity extends ActionBarActivity {
 
     private TextView textViewTotalColor;
     private ImageView cameraView;
+    private ImageView equalizedView;
     private Button cameraButton;
     private SeekBar pixelSeekBar;
     private SeekBar seekBar2;
     private Bitmap currentImageBitmap;
+    private Boolean pictureTaken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        pictureTaken = false;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         cameraView = (ImageView)findViewById(R.id.cameraImage);
+        equalizedView = (ImageView)findViewById(R.id.equalizedImage);
         textViewTotalColor = (TextView)findViewById(R.id.textViewTotalWarna);
 
         pixelSeekBar = (SeekBar)findViewById(R.id.pixelSeekBar);
@@ -47,7 +52,12 @@ public class MainActivity extends ActionBarActivity {
         pixelSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
+                if(pictureTaken) {
+                    System.out.println("masuk kok");
+                    int progressChange = (int) (seekBar.getProgress() * 1000000);
+                    int progressChange2 = (int) (seekBar2.getProgress() * 1000000);
+                    equalizedView.setImageBitmap(ImageProcessor.histogramEqualization(currentImageBitmap, progressChange, progressChange2));
+                }
             }
 
             @Override
@@ -57,9 +67,6 @@ public class MainActivity extends ActionBarActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                //algoritma gantinya disini
-                int progressChange = (int)(seekBar.getProgress() * 255);
-                
             }
         });
     }
@@ -91,10 +98,13 @@ public class MainActivity extends ActionBarActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            currentImageBitmap = (Bitmap) extras.get("data");
+            Bitmap tempBitmap = (Bitmap) extras.get("data");
+            currentImageBitmap = ImageProcessor.toGrayscale(tempBitmap);
             //System.out.println("pixelnya : " + currentImageBitmap.getPixel(0, 0));
             cameraView.setImageBitmap(currentImageBitmap);
+            equalizedView.setImageBitmap(ImageProcessor.histogramEqualization(currentImageBitmap,0,0));
             textViewTotalColor.setText("Jumlah Warna : " + ImageProcessor.countTotalColor(currentImageBitmap));
+            pictureTaken = true;
         }
     }
 
